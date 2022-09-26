a7715276dd72c1e71eaf4baaff72419e0feddc5c
Vladislav Tolkachev <wlad.tolkachew@yandex.by>
2022-09-27 01:31:59 +0300

    FavSong

---
+++ src/FavoriteSong.java
@@ -0,0 +1,14 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Song - Glimpse of Us");
+        System.out.println("Artist - Joji");
+        System.out.println("Text: \n'Cause sometimes I look in her eyes\n" +
+                "And that's where I find a glimpse of us\n" +
+                "And I try to fall for her touch\n" +
+                "But I'm thinking of the way it was\n" +
+                "Said I'm fine and said I moved on\n" +
+                "I'm only here passing time in her arms\n" +
+                "Hoping I'll find\n" +
+                "A glimpse of us ");
+    }
+}
